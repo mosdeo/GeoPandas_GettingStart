11ba2da63ce53197ebc88b3df6dfaab696912c01
Lin, Kao-Yuan <mosdeo@gmail.com>
2021-04-27 18:17:48 +0800

    backup before delete

---
+++ China.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import geopandas as gp
+import numpy as np
+
+print("test")
+# villages_shp = gp.read_file("C:\\Users\\mosde\\Downloads\\China_Cities_Towns\\China_Cities_Towns.shp") #全台灣村里界圖
+# # taichung_shp = villages_shp.query('COUNTYNAME=="臺中市"') #臺中市村里界圖
+
+# print("test")	
+# villages_shp.plot(
+#     cmap=plt.cm.Blues, #指定顏色
+#     # column='dummy number' #指定從自身的這個 column 讀取顏色深度
+# )
+# print("test")
+# plt.show()
